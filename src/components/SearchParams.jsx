58c4360553634b24cab7758060d7401b50cba5a9
iannech <iaoniango94@gmail.com>
2023-02-07 22:57:02 +0300

    Mapping through data with hooks

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "reptile", "rabbit", "cat", "dog"]
+
 const SearchParams = () => {
     // setState Hook
     const [location, setLocation] = useState("");
-
+    const [animal, setAnimal] = useState("")
     return(
         <div className="search-params">
             <form>
@@ -14,6 +16,18 @@ const SearchParams = () => {
                         id="location" 
                         value={location} 
                         placeholder="Location"/>
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select 
+                        id="animal"
+                        value={animal}
+                        onChange = {(e) => {setAnimal(e.target.value)}}>
+                        <option/>
+                        {ANIMALS.map((animal) => (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
