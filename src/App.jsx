bf1607402c62c488f6eb4ce54387a294e47d9973
iannech <iaoniango94@gmail.com>
2023-02-13 22:06:38 +0300

    Working with React router [Browser Router]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./components/Details";
 import SearchParams from "./components/SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <div>
+        <h1>Adopt me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
