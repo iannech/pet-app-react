f7b142cccdf77ee81694aa244900d4f8c94a7b90
iannech <iaoniango94@gmail.com>
2023-02-13 20:48:10 +0300

    offload results to individual component to prevent bloating of SearchParams

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./components/SearchParams";
 
 const App = () => {
-  return(
-  <div>
-    <h1>Adopt me!</h1>
-    <SearchParams/>
-  </div>
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <SearchParams />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
+root.render(<App />);
--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,81 +1,86 @@
-import { useState, useEffect} from "react";
-import Pet from "./Pet";
+import { useState, useEffect } from "react";
+import useBreedList from "../UseBreedList";
+import Results from "./Results";
 
-const ANIMALS = ["bird", "reptile", "rabbit", "cat", "dog"]
-const breeds = [];
+const ANIMALS = ["bird", "reptile", "rabbit", "cat", "dog"];
 
 const SearchParams = () => {
-    // setState Hook
-    const [pets, setPets] = useState([]);
-    const [location, setLocation] = useState("");
-    const [animal, setAnimal] = useState("");
-    const [breed, setBreed] = useState("");
+  // setState Hook
+  const [pets, setPets] = useState([]);
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [breeds] = useBreedList(animal);
 
-    useEffect(() => {
-        requestPets();
+  useEffect(() => {
+    requestPets();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+  }, []);
 
-    async function requestPets(){
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-        );
-        const json = await res.json();
-        setPets(json.pets)
-    }
-
-    return(
-        <div className="search-params">
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                requestPets();
-            }}>
-                <label htmlFor="location">
-                    Location
-                    <input 
-                        onChange={(e) => setLocation(e.target.value)}
-                        id="location" 
-                        value={location} 
-                        placeholder="Location"/>
-                </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select 
-                        id="animal"
-                        value={animal}
-                        onChange = {(e) => {
-                            setAnimal(e.target.value)
-                            setBreed("")}}>
-                        <option/>
-                        {ANIMALS.map((animal) => (
-                            <option key={animal}>{animal}</option>
-                        ))}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    Breed
-                    <select
-                        id="breed"
-                        value={breed}
-                        disabled={breeds.length === 0} // disable if breeds is 0
-                        onChange = {(e) => {setBreed(e.target.value)}}>
-                            <option/>
-                                {breeds.map((breed) => (<option key={breed}>{breed}</option>))}
-                        </select>
-                </label>
-                <button>Submit</button>
-            </form>
-            {
-                pets.map((pet) => (
-                    <Pet 
-                        name={pet.name} 
-                        animal={pet.animal} 
-                        breed={pet.breed} 
-                        key={pet.id}/>
-                ))
-            }
-        </div>
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
+    const json = await res.json();
+    setPets(json.pets);
+  }
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            disabled={breeds.length === 0} // disable if breeds is 0
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
 };
 
 export default SearchParams;
