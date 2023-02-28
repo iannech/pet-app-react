50f20bdd761550655ee5d731075cee3af603c298
iannech <iaoniango94@gmail.com>
2023-02-28 21:49:38 +0300

    handling errors using ErrorBoundary class component

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import fetchPet from "../fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Details = () => {
   const { id } = useParams();
@@ -31,4 +32,13 @@ const Details = () => {
   );
 };
 
-export default Details;
+// only catches errors from components that are inside of it
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
