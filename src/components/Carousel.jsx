849e535895987713ecce13cc21b9759b4b7a48ac
iannech <iaoniango94@gmail.com>
2023-02-28 21:03:31 +0300

    handling events in class components

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+// explore class components: no hooks, lifecycle mgmt useeffect replaced with componentDid...
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  // function to change carousel image
+  // must be arrow function so that state is passed unchanged during invocation
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index, // everything in dom is string, + converts to int
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal-hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
